3a18abc4447f3f09325e513fe852f9f7b1f51123
Denito37 <69406178+Denito37@users.noreply.github.com>
2020-09-14 23:49:09 -0400

    Add files via upload

--- Data analyzer.py
+++ Data analyzer.py
@@ -1,13 +1,11 @@
-#data analyzer, organizes data from csv files
-# add visual graph with matplotlib
-# make program read other types of files
+#data analyzer, organizes data from csv files, and creates a histograph png
 import matplotlib.pyplot as plt
 import pandas as pd
 
 data = input('Enter csv file name:')
-datacsv = str(data)
+datafile = str(data)
 
-num = pd.read_csv(datacsv)
+num = pd.read_csv(datafile)
 
 print('Min:' , num.min())
 print('Max:' , num.max())
@@ -15,5 +13,7 @@ print('Mean:', num.mean())
 print('Median:', num.median())
 print('Standard Deviation:', num.std())
 
+num.plot.hist()
+plt.savefig('test.png')
 
 
